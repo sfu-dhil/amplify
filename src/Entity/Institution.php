b5768cb4d7b98558b15e466ce759d4fb5e3270e3
Michael Joyce <ubermichael@gmail.com>
2020-08-25 14:35:38 -0700

    shorten the province and name fields in institution.

--- src/Entity/Institution.php
+++ src/Entity/Institution.php
@@ -20,13 +20,13 @@ class Institution extends AbstractEntity {
 
     /**
      * @var string
-     * @ORM\Column(type="string", length=64, nullable=false)
+     * @ORM\Column(type="string", length=40, nullable=false)
      */
     private $province;
 
     /**
      * @var string
-     * @ORM\Column(type="string", length=200, nullable=false)
+     * @ORM\Column(type="string", length=100, nullable=false)
      */
     private $name;
 
