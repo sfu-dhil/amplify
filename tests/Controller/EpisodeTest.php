c8331732627a71040809ffd002c3b5c477e395f5
Michael Joyce <mjoyce@sfu.ca>
2022-02-11 16:39:12 -0800

    add tests for image files in episodes

--- tests/Controller/EpisodeTest.php
+++ tests/Controller/EpisodeTest.php
@@ -12,7 +12,9 @@ namespace App\Tests\Controller;
 
 use App\Repository\EpisodeRepository;
 use Nines\MediaBundle\Repository\AudioRepository;
+use Nines\MediaBundle\Repository\ImageRepository;
 use Nines\MediaBundle\Service\AudioManager;
+use Nines\MediaBundle\Service\ImageManager;
 use Nines\UserBundle\DataFixtures\UserFixtures;
 use Nines\UtilBundle\TestCase\ControllerTestCase;
 use Symfony\Component\HttpFoundation\Response;
@@ -252,9 +254,9 @@ class EpisodeTest extends ControllerTestCase {
         $preCount = count($repo->findAll());
 
         $this->login(UserFixtures::ADMIN);
-        $crawler = $this->client->request('GET', '/episode/4');
+        $crawler = $this->client->request('GET', '/episode/3');
         $this->assertResponseIsSuccessful();
-        $form = $crawler->filter('form[action="/episode/4"]')->form();
+        $form = $crawler->filter('form[action="/episode/3"]')->form();
         $this->client->submit($form);
 
         $this->assertResponseRedirects('/episode/', Response::HTTP_FOUND);
@@ -372,6 +374,122 @@ class EpisodeTest extends ControllerTestCase {
 
         $form = $crawler->filter('form.delete-form[action="/episode/4/delete_audio/4"]')->form();
         $form->getNode()->setAttribute('action', '/episode/3/delete_audio/4');
+
+        $this->client->submit($form);
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+
+        $this->em->clear();
+        $postCount = count($repo->findAll());
+        $this->assertSame($preCount, $postCount);
+    }
+
+
+    public function testAnonNewImage() : void {
+        $crawler = $this->client->request('GET', '/episode/1/new_image');
+        $this->assertResponseRedirects('/login', Response::HTTP_FOUND);
+    }
+
+    public function testUserNewImage() : void {
+        $this->login(UserFixtures::USER);
+        $crawler = $this->client->request('GET', '/episode/1/new_image');
+        $this->assertSame(403, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testAdminNewImage() : void {
+        $this->login(UserFixtures::ADMIN);
+        $crawler = $this->client->request('GET', '/episode/1/new_image');
+        $this->assertResponseIsSuccessful();
+
+        $manager = self::$container->get(ImageManager::class);
+        $manager->setCopy(true);
+
+        $form = $crawler->selectButton('Create')->form([
+            'image[public]' => 1,
+            'image[description]' => 'Description',
+            'image[license]' => 'License',
+        ]);
+        $form['image[file]']->upload(dirname(__FILE__, 2) . '/data/image/28213926366_4430448ff7_c.jpg');
+        $this->client->submit($form);
+        $this->assertResponseRedirects('/episode/1');
+        $responseCrawler = $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $manager->setCopy(false);
+    }
+
+    public function testAnonEditImage() : void {
+        $crawler = $this->client->request('GET', '/episode/1/edit_image/1');
+        $this->assertResponseRedirects('/login', Response::HTTP_FOUND);
+    }
+
+    public function testUserEditImage() : void {
+        $this->login(UserFixtures::USER);
+        $crawler = $this->client->request('GET', '/episode/1/edit_image/1');
+        $this->assertSame(403, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testAdminEditImage() : void {
+        $this->login(UserFixtures::ADMIN);
+        $crawler = $this->client->request('GET', '/episode/1/edit_image/1');
+        $this->assertResponseIsSuccessful();
+
+        $manager = self::$container->get(ImageManager::class);
+        $manager->setCopy(true);
+
+        $form = $crawler->selectButton('Update')->form([
+            'image[public]' => 0,
+            'image[description]' => 'Updated Description',
+            'image[license]' => 'Updated License',
+        ]);
+        $form['image[newFile]']->upload(dirname(__FILE__, 2) . '/data/image/3632486652_b432f7b283_c.jpg');
+        $this->client->submit($form);
+        $this->assertResponseRedirects('/episode/1');
+        $responseCrawler = $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $manager->setCopy(false);
+    }
+
+    public function testAnonDeleteImage() : void {
+        $crawler = $this->client->request('DELETE', '/episode/1/delete_image/1');
+        $this->assertResponseRedirects('/login', Response::HTTP_FOUND);
+    }
+
+    public function testUserDeleteImage() : void {
+        $this->login(UserFixtures::USER);
+        $crawler = $this->client->request('DELETE', '/episode/1/delete_image/1');
+        $this->assertSame(403, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testAdminDeleteImage() : void {
+        $repo = self::$container->get(ImageRepository::class);
+        $preCount = count($repo->findAll());
+
+        $this->login(UserFixtures::ADMIN);
+        $crawler = $this->client->request('GET', '/episode/4');
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->filter('form.delete-form[action="/episode/4/delete_image/4"]')->form();
+        $this->client->submit($form);
+        $this->assertResponseRedirects('/episode/4');
+        $responseCrawler = $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $this->em->clear();
+        $postCount = count($repo->findAll());
+        $this->assertSame($preCount - 1, $postCount);
+    }
+
+    public function testAdminDeleteWrongImage() : void {
+        $repo = self::$container->get(ImageRepository::class);
+        $preCount = count($repo->findAll());
+
+        $this->login(UserFixtures::ADMIN);
+        $crawler = $this->client->request('GET', '/episode/4');
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->filter('form.delete-form[action="/episode/4/delete_image/4"]')->form();
+        $form->getNode()->setAttribute('action', '/episode/3/delete_image/4');
 
         $this->client->submit($form);
         $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
