8c53ccf1dcb5bcd25c5d620736bfa0be8ad3d554
Michael Joyce <mjoyce@sfu.ca>
2022-06-15 14:34:51 -0700

    php-cs-fixer

--- migrations/2022/06/Version20220615185339.php
+++ migrations/2022/06/Version20220615185339.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+/*
+ * (c) 2022 Michael Joyce <mjoyce@sfu.ca>
+ * This source file is subject to the GPL v2, bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -10,15 +16,12 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220615185339 extends AbstractMigration
-{
-    public function getDescription(): string
-    {
+final class Version20220615185339 extends AbstractMigration {
+    public function getDescription() : string {
         return '';
     }
 
-    public function up(Schema $schema): void
-    {
+    public function up(Schema $schema) : void {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE category CHANGE name name VARCHAR(191) NOT NULL, CHANGE label label VARCHAR(200) NOT NULL');
         $this->addSql('ALTER TABLE contributor_role CHANGE name name VARCHAR(191) NOT NULL, CHANGE label label VARCHAR(200) NOT NULL');
@@ -82,8 +85,7 @@ final class Version20220615185339 extends AbstractMigration
         $this->addSql('ALTER TABLE season CHANGE alternative_title sub_title VARCHAR(255) DEFAULT NULL');
     }
 
-    public function down(Schema $schema): void
-    {
+    public function down(Schema $schema) : void {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE category CHANGE name name VARCHAR(120) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE label label VARCHAR(120) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE contributor_role CHANGE name name VARCHAR(120) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE label label VARCHAR(120) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
--- migrations/2022/06/Version20220615212244.php
+++ migrations/2022/06/Version20220615212244.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+/*
+ * (c) 2022 Michael Joyce <mjoyce@sfu.ca>
+ * This source file is subject to the GPL v2, bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -10,21 +16,17 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220615212244 extends AbstractMigration
-{
-    public function getDescription(): string
-    {
+final class Version20220615212244 extends AbstractMigration {
+    public function getDescription() : string {
         return '';
     }
 
-    public function up(Schema $schema): void
-    {
+    public function up(Schema $schema) : void {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE episode DROP copyright');
     }
 
-    public function down(Schema $schema): void
-    {
+    public function down(Schema $schema) : void {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE episode ADD copyright LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
